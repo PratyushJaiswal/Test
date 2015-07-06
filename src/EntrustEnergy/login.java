76d27503cf46383851baaf8ca102fb69c96a46be
pratyush.jaiswal <pratyush.jaiswal@SSD-16-0065.corp.softwaysolutions.com>
2015-07-06 13:20:45 +0530

    First comit

---
+++ src/EntrustEnergy/login.java
@@ -0,0 +1,28 @@
+package EntrustEnergy;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+
+import org.testng.annotations.Test;
+
+public class login extends EntrustEnergy.OpenBrowser  {
+	
+	@Test
+	public static void logIn() throws Exception{
+	
+		driver.findElement(By.xpath(".//*[@id='login-email']")).sendKeys("softway_admin");
+		driver.findElement(By.xpath(".//*[@id='login-password']")).sendKeys("softway.123456");
+		driver.findElement(By.xpath(".//*[@id='form-login']/div[4]/div/button")).click();
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		String ActualTitle = driver.getTitle();
+		Assert.assertEquals(ActualTitle, "AIM Administrator :: Control Center");
+		
+		
+		
+	
+		
+	}
+
+}
